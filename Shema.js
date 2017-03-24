28226803c061da2b4432cf9350d8d7763aa18980
DiRaiks <t.fane@mail.ru>
2017-03-24 18:52:30 +0300

    change Shema

--- Shema.js
+++ Shema.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var config = require('./config'); //файл конфигурации
 var db = mongoose.createConnection('mongodb://' + config.db.host + '/' + config.db.name);
 
 //первый счетчик - для удобство поиска по базе и обнавления значения.
